b74fbcabf219a626663031c7447ad3479c510fdf
Saswat_Sharma <saswatashu@gmail.com>
2022-08-21 00:02:56 +0530

    Diff.java added

---
+++ diff.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class diff {
+    public static void main(String []args)
+{
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter two numbers");
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int dif=a-b;
+    System.out.println("Diff is"+dif) ;
+}
+}
