ca8c58a7a329cbc56bf1a22eb0bf3a9047043761
Saswat_Sharma <saswatashu@gmail.com>
2022-08-23 23:12:29 +0530

    DIVIDE.JAVA ADDED

---
+++ divide.java
@@ -0,0 +1,13 @@
+import java.util.*;
+    public class divide
+    {
+     public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 2 numbers");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=a/b;
+        System.out.println(" Result ="+c);
+    }
+}
